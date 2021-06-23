a0cf75852a20c635a228cb39626f598c0b91da30
HJT-hl <hlalongweb@163.com>
2021-06-23 08:22:03 +0800

    update

--- type.d.ts
+++ type.d.ts
@@ -1,5 +1,5 @@
 declare module "erwin"  {
-    export function observe<T>(observer:Record<string,unknown>):T
+    export function observe<T>(observer:Record<string,unknown>):(tag:unknown)=>T
     export namespace config {
         var funReg : RegExp;
         var private : boolean;
